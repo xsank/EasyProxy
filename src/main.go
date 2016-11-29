376ddf98b7d1e146ea6dcb22d75b4023297abb62
xsank.mz <xsank.mz@alibaba-inc.com>
2016-11-29 20:41:52 +0800

    to use mulicore for io intensive

--- src/main.go
+++ src/main.go
@@ -7,15 +7,22 @@ import (
 	"path/filepath"
 	"github.com/xsank/EasyProxy/src/log"
 	"github.com/xsank/EasyProxy/src/web"
+	"runtime"
 )
 
-const DefaultConfigFile = "conf/default.json"
-const DefaultLogFile = "esayproxy.log"
+const
+(
+	DefaultConfigFile = "conf/default.json"
+	DefaultLogFile = "esayproxy.log"
+)
 
 func main() {
+	runtime.GOMAXPROCS(2 * runtime.NumCPU())
+
 	homePath := util.HomePath()
 	log.Init(DefaultLogFile)
 	config, err := config.Load(filepath.Join(homePath, DefaultConfigFile))
+
 	if err == nil {
 		webServer := new(web.WebServer)
 		webServer.Init(config)
